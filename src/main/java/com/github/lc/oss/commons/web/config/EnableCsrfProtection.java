fd47adbeb468d775389da9165d27a9a800fef175
Seth Locke-Chappel <92889395+locke-chappel@users.noreply.github.com>
2022-12-14 18:45:55 +0000

    Push from SCM (2022-12-14)

--- src/main/java/com/github/lc/oss/commons/web/config/EnableCsrfProtection.java
+++ src/main/java/com/github/lc/oss/commons/web/config/EnableCsrfProtection.java
@@ -1,16 +1,8 @@
 package com.github.lc.oss.commons.web.config;
 
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-
-public class EnableCsrfProtection implements Condition {
+public class EnableCsrfProtection extends PropertyConditional {
     @Override
-    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-        String property = context.getEnvironment().getProperty("application.security.enableCsrfProtection");
-        if (property == null) {
-            return true;
-        }
-        return !property.toLowerCase().equals("false");
+    protected String getProperty() {
+        return "application.security.enableCsrfProtection";
     }
 }
