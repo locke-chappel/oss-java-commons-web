9c1eb5ce731f3f1d66e4407ac85a6bf4f3649807
Seth Locke-Chappel <92889395+locke-chappel@users.noreply.github.com>
2023-01-20 19:33:56 +0000

    Push from SCM (2023-01-20)

---
+++ src/test/java/com/github/lc/oss/commons/web/hints/AbstractRuntimeHintsRegistrarTest.java
@@ -0,0 +1,34 @@
+package com.github.lc.oss.commons.web.hints;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.aot.hint.ReflectionHints;
+import org.springframework.aot.hint.RuntimeHints;
+
+import com.github.lc.oss.commons.testing.AbstractMockTest;
+
+public class AbstractRuntimeHintsRegistrarTest extends AbstractMockTest {
+    private static class TestClass extends AbstractRuntimeHintsRegistrar {
+        @Override
+        protected Set<Class<?>> getClasses() {
+            return new HashSet<>(Arrays.asList(TestClass.class));
+        }
+    }
+
+    @Test
+    public void test_registerHints() {
+        AbstractRuntimeHintsRegistrar registrar = new TestClass();
+
+        RuntimeHints hints = Mockito.mock(RuntimeHints.class);
+        ClassLoader classLoader = Mockito.mock(ClassLoader.class);
+        ReflectionHints reflection = Mockito.mock(ReflectionHints.class);
+
+        Mockito.when(hints.reflection()).thenReturn(reflection);
+
+        registrar.registerHints(hints, classLoader);
+    }
+}
