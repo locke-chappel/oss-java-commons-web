73dafb90e62d37e712fc62aaf32bcbad48c17cfc
Seth Locke-Chappel <92889395+locke-chappel@users.noreply.github.com>
2023-07-18 18:35:25 +0000

    Push from SCM (2023-07-18)

--- src/main/java/io/github/lc/oss/commons/web/hints/AbstractRuntimeHintsRegistrar.java
+++ src/main/java/io/github/lc/oss/commons/web/hints/AbstractRuntimeHintsRegistrar.java
@@ -1,20 +1,83 @@
 package io.github.lc.oss.commons.web.hints;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.aot.hint.TypeReference;
 
 public abstract class AbstractRuntimeHintsRegistrar implements RuntimeHintsRegistrar {
-    protected abstract Set<Class<?>> getClasses();
+    private static final Set<TypeReference> NO_REFLECTIONS = Collections.unmodifiableSet(new HashSet<>(0));
+    private static final Set<TypeReference> DEFAULT_REFLECTIONS = Collections.unmodifiableSet(new HashSet<>(Arrays.asList( //
+            TypeReference.of(io.github.lc.oss.commons.util.PathNormalizer.class), //
+            TypeReference.of(io.github.lc.oss.commons.web.advice.AbstractControllerAdvice.class), //
+            TypeReference.of(io.github.lc.oss.commons.web.advice.AbstractSystemIdentityAdvice.class), //
+            TypeReference.of(io.github.lc.oss.commons.web.advice.CommonAdvice.class), //
+            TypeReference.of(io.github.lc.oss.commons.web.advice.ETagAdvice.class), //
+            TypeReference.of(io.github.lc.oss.commons.web.annotations.SystemIdentity.class), //
+            TypeReference.of(io.github.lc.oss.commons.web.annotations.HttpCachable.class), //
+            TypeReference.of(org.springframework.stereotype.Controller.class), //
+            TypeReference.of(org.springframework.web.bind.annotation.RestController.class), //
+            TypeReference.of(org.springframework.web.bind.annotation.RequestMapping.class), //
+            TypeReference.of(org.springframework.web.bind.annotation.GetMapping.class), //
+            TypeReference.of(org.springframework.web.bind.annotation.PostMapping.class), //
+            TypeReference.of(org.springframework.web.bind.annotation.PatchMapping.class), //
+            TypeReference.of(org.springframework.web.bind.annotation.PutMapping.class), //
+            TypeReference.of(org.springframework.web.bind.annotation.DeleteMapping.class) //
+    )));
+
+    private static final Set<List<TypeReference>> NO_PROXIES = Collections.unmodifiableSet(new HashSet<>(0));
+
+    private static final Set<String> NO_RESOURCE_BUNDLES = Collections.unmodifiableSet(new HashSet<>(0));
+    private static final Set<String> DEFAULT_RESOURCE_BUNDLES = Collections.unmodifiableSet(new HashSet<>(Arrays.asList( //
+            "org.aspectj.weaver.weaver-messages", //
+            "com.google.javascript.jscomp.parsing.ParserConfig")));
+
+    protected Set<TypeReference> getDefaultReflectionClasses() {
+        return AbstractRuntimeHintsRegistrar.DEFAULT_REFLECTIONS;
+    }
+
+    protected Set<TypeReference> getReflectionClasses() {
+        return AbstractRuntimeHintsRegistrar.NO_REFLECTIONS;
+    }
+
+    protected Set<List<TypeReference>> getProxyClasses() {
+        return AbstractRuntimeHintsRegistrar.NO_PROXIES;
+    }
+
+    protected Set<String> getDefaultResourceBundles() {
+        return AbstractRuntimeHintsRegistrar.DEFAULT_RESOURCE_BUNDLES;
+    }
+
+    protected Set<String> getResourceBundles() {
+        return AbstractRuntimeHintsRegistrar.NO_RESOURCE_BUNDLES;
+    }
 
     @Override
     public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-        for (MemberCategory mc : MemberCategory.values()) {
-            for (Class<?> clazz : this.getClasses()) {
-                hints.reflection().registerType(clazz, mc);
-            }
+        for (TypeReference ref : this.getDefaultReflectionClasses()) {
+            hints.reflection().registerType(ref, MemberCategory.values());
+        }
+
+        for (TypeReference ref : this.getReflectionClasses()) {
+            hints.reflection().registerType(ref, MemberCategory.values());
+        }
+
+        for (List<TypeReference> proxy : this.getProxyClasses()) {
+            hints.proxies().registerJdkProxy(proxy.toArray(new TypeReference[proxy.size()]));
+        }
+
+        for (String bundle : this.getDefaultResourceBundles()) {
+            hints.resources().registerResourceBundle(bundle);
+        }
+
+        for (String bundle : this.getResourceBundles()) {
+            hints.resources().registerResourceBundle(bundle);
         }
     }
 }
--- src/test/java/io/github/lc/oss/commons/web/hints/AbstractRuntimeHintsRegistrarTest.java
+++ src/test/java/io/github/lc/oss/commons/web/hints/AbstractRuntimeHintsRegistrarTest.java
@@ -2,33 +2,66 @@ package io.github.lc.oss.commons.web.hints;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.aot.hint.ProxyHints;
 import org.springframework.aot.hint.ReflectionHints;
+import org.springframework.aot.hint.ResourceHints;
 import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.TypeReference;
 
 import io.github.lc.oss.commons.testing.AbstractMockTest;
 
 public class AbstractRuntimeHintsRegistrarTest extends AbstractMockTest {
-    private static class TestClass extends AbstractRuntimeHintsRegistrar {
-        @Override
-        protected Set<Class<?>> getClasses() {
-            return new HashSet<>(Arrays.asList(TestClass.class));
-        }
+
+    @Test
+    public void test_registerHints_nothing() {
+        AbstractRuntimeHintsRegistrar registrar = new AbstractRuntimeHintsRegistrar() {
+        };
+
+        RuntimeHints runtimeHints = Mockito.mock(RuntimeHints.class);
+        ClassLoader classLoader = Mockito.mock(ClassLoader.class);
+        ReflectionHints reflectionHints = Mockito.mock(ReflectionHints.class);
+        ResourceHints resourceHints = Mockito.mock(ResourceHints.class);
+
+        Mockito.when(runtimeHints.reflection()).thenReturn(reflectionHints);
+        Mockito.when(runtimeHints.resources()).thenReturn(resourceHints);
+
+        registrar.registerHints(runtimeHints, classLoader);
     }
 
     @Test
     public void test_registerHints() {
-        AbstractRuntimeHintsRegistrar registrar = new TestClass();
+        AbstractRuntimeHintsRegistrar registrar = new AbstractRuntimeHintsRegistrar() {
+            @Override
+            protected Set<TypeReference> getReflectionClasses() {
+                return new HashSet<>(Arrays.asList(TypeReference.of(AbstractRuntimeHintsRegistrar.class)));
+            }
 
-        RuntimeHints hints = Mockito.mock(RuntimeHints.class);
+            @Override
+            protected Set<List<TypeReference>> getProxyClasses() {
+                return new HashSet<>(Arrays.asList(Arrays.asList(TypeReference.of(AbstractRuntimeHintsRegistrar.class))));
+            }
+
+            @Override
+            protected Set<String> getResourceBundles() {
+                return new HashSet<>(Arrays.asList("res-bundle"));
+            }
+        };
+
+        RuntimeHints runtimeHints = Mockito.mock(RuntimeHints.class);
         ClassLoader classLoader = Mockito.mock(ClassLoader.class);
-        ReflectionHints reflection = Mockito.mock(ReflectionHints.class);
+        ReflectionHints reflectionHints = Mockito.mock(ReflectionHints.class);
+        ProxyHints proxyHints = Mockito.mock(ProxyHints.class);
+        ResourceHints resourceHints = Mockito.mock(ResourceHints.class);
 
-        Mockito.when(hints.reflection()).thenReturn(reflection);
+        Mockito.when(runtimeHints.reflection()).thenReturn(reflectionHints);
+        Mockito.when(runtimeHints.proxies()).thenReturn(proxyHints);
+        Mockito.when(runtimeHints.resources()).thenReturn(resourceHints);
 
-        registrar.registerHints(hints, classLoader);
+        registrar.registerHints(runtimeHints, classLoader);
     }
 }
